3f94c1a2e32cdb8272a31aa80a663ca476c61a40
Jenna Perez <jmsp43@gmail.com>
2023-03-25 13:46:57 -0400

    snake is changing direction up/down but not right/left yet

--- script.js
+++ script.js
@@ -27,9 +27,12 @@ let dx = 10;
 //dy is vertical change in velocity
 let dy = 0;
 
+let changingDirection = false;
 
 
+///// EVENT LISTENER /////
 
+document.addEventListener('keydown', changeDirection)
 
 
 //////////// FUNCTIONS ///////////////
@@ -38,6 +41,7 @@ let dy = 0;
 
 //to display snake on board, need a func to draw snake and then one that prints snake
 
+
 //this draws each sqaure that makes up the snake individually
 function drawSnake(snakeSquare) {
   boardCtx.fillStyle = snakeColor;
@@ -45,11 +49,12 @@ function drawSnake(snakeSquare) {
   boardCtx.fillRect(snakeSquare.x, snakeSquare.y, 10, 10);
   boardCtx.strokeRect(snakeSquare.x, snakeSquare.y, 10, 10);
 }
-
 //prints each sqaure drawn by drawSnake() in succession
 function printSnake() {
   snake.forEach(drawSnake);
 }
+
+
 
 //draws blank board with no snake
 function clearBoard() {
@@ -58,6 +63,8 @@ function clearBoard() {
   boardCtx.fillRect(0, 0, board.width, board.height);
   boardCtx.strokeRect(0, 0, board.width, board.height);
 }
+
+
 
 //updates position of the snake
 function moveSnake() {
@@ -70,6 +77,46 @@ function moveSnake() {
 }
 //snake moves 1 step to right (by 10px), increase x coordinate of every part of the snake by 10px (aka dx = +10)
 //snake moves 1 step to left (by 10px), decrease y coordinate of every part of the snake by 10px (aka dx = -10)
+
+//changes direction of snake
+function changeDirection(event) {
+  //keys apparently have set key codes
+  const left = 37
+  const up = 38
+  const right = 39
+  const down = 40
+
+  //prevents changing direction when you are already doing so
+  if (changingDirection) return;
+  changingDirection = true;
+
+  const keyPressed = event.keyCode;
+  //think of x and y axis
+  const goingLeft = dx === -10
+  const goingUp = dy === -10
+  const goingRight = dx === 10
+  const goingDown = dy === 10
+
+  if (keyPressed === left && goingRight === false) {
+      dx = -10
+      dy = 0
+  }
+  if (keyPressed === up && goingDown === false) {
+      dx = 0
+      dy = -10
+  }
+  if (keyPressed === right && goingLeft === false) {
+      dx = 10
+      dy = 0
+  }
+  if (keyPressed === down && goingUp === false) {
+      dx = 0
+      dy = 10
+  }
+}
+
+
+
 
 function runGame() {
   //setTimeout between each movement of snake so user can see what is happening with each movement as opposed to snake just jumping forward
